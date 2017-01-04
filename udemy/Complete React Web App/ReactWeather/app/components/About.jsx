14b6377f83195c334068974ef95b7bb9ef7865d8
kazbek-d <kazbek_d@hotmail.com>
2017-01-04 18:39:24 +0300

    Stype about page & link to resources

--- udemy/Complete React Web App/ReactWeather/app/components/About.jsx
+++ udemy/Complete React Web App/ReactWeather/app/components/About.jsx
@@ -1,7 +1,26 @@
 var React = require('react');
 
 var About = (props) => (
-    <h3>About component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application build on a React. I have built this
+        for The Complete React Web App Developor Course.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the
+            javascript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used
+            Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
   );
 
 module.exports = About;
