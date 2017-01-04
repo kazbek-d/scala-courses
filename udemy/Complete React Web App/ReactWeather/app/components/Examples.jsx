f18a81fcc8a5dad01e983b4080b594ddc1baa43f
kazbek-d <kazbek_d@hotmail.com>
2017-01-04 18:20:56 +0300

    Grid

--- udemy/Complete React Web App/ReactWeather/app/components/Examples.jsx
+++ udemy/Complete React Web App/ReactWeather/app/components/Examples.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => (
-  <h3>Examples component</h3>
+  <div>
+    <h1 className="text-center">Examples</h1>
+    <p>Here are a few examples to try out:</p>
+    <ol>
+      <li>
+        <Link to="/?location=Moscow">Moscow</Link>
+      </li>
+      <li>
+        <Link to="/?location=Ramenskoye">Ramenskoye</Link>
+      </li>
+    </ol>
+  </div>
 );
 
 module.exports = Examples;
