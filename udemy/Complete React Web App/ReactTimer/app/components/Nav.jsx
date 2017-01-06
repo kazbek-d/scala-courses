485609ff3543960f2bd15727e993e006bae8a809
kazbek-d <kazbek_d@hotmail.com>
2017-01-07 00:31:16 +0300

    Init React Timer App

---
+++ udemy/Complete React Web App/ReactTimer/app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function(){
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ui className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+            </li>
+          </ui>
+        </div>
+        <div className="top-bar-right">
+          <ui className="menu">
+            <li className="menu-text">Created by 
+              <a href="https://github.com/kazbek-d/scala-courses/tree/master/udemy/Complete%20React%20Web%20App" target="_blank">Kazbek Dzarasov</a>
+            </li>
+          </ui>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
