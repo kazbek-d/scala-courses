b5ed3d05c22db3cdee53b4002f3380443e5849cc
kazbek-d <kazbek_d@hotmail.com>
2017-01-29 00:41:40 +0300

    Add navigation

---
+++ udemy/Complete React Web App/ReactTimer/app/components/Countdown.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Countdown = (props) => (
+  <div>
+    Countdown Page
+  </div>
+);
+
+module.exports = Countdown;
---
+++ udemy/Complete React Web App/ReactTimer/app/components/Timer.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Timer = (props) => (
+  <div>
+    Timer Page
+  </div>
+);
+
+module.exports = Timer;
