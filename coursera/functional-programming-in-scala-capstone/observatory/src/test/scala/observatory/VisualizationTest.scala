f95d2bac3775b6f12954b384a31a524c829e15a1
kazbek-d <kazbek_d@hotmail.com>
2017-03-29 14:33:36 +0300

    Fix Visualization.greatCircleDistance

--- coursera/functional-programming-in-scala-capstone/observatory/src/test/scala/observatory/VisualizationTest.scala
+++ coursera/functional-programming-in-scala-capstone/observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -9,5 +9,25 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
 
+  test("predictTemperature 0") {
+    val result = Visualization.predictTemperature(List.empty, Location(90.0,-176.0))
+    assert(result === 0.0)
+  }
+
+  test("predictTemperature 10") {
+    val result = Visualization.predictTemperature(List((Location(0.0,0.0),10.0)), Location(90.0,-176.0))
+    assert(result === 10.0)
+  }
+
+  test("predictTemperature Zero distance") {
+    val result = Visualization.predictTemperature(List((Location(0.0,0.0),10.0)), Location(0.0,0.0))
+    assert(result === 10.0)
+  }
+
+  test("predictTemperature Zero distance 2") {
+    val result = Visualization.predictTemperature(List((Location(45.0,-90.0),10.0),(Location(-45.0,0.0),20.0)), Location(45.0,-90.0))
+    assert(result === 10.0)
+  }
+
 
 }
