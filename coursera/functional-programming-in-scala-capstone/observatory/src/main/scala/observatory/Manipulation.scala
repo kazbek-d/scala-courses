3dbc007dddf0c91b175f11a755e42d3c52a9e4ca
Kazbek Dzarasov <kazbek_d@hotmail.com>
2017-04-09 01:18:29 +0300

    Implement Manipulation methods

--- coursera/functional-programming-in-scala-capstone/observatory/src/main/scala/observatory/Manipulation.scala
+++ coursera/functional-programming-in-scala-capstone/observatory/src/main/scala/observatory/Manipulation.scala
@@ -10,9 +10,8 @@ object Manipulation {
     * @return A function that, given a latitude in [-89, 90] and a longitude in [-180, 179],
     *         returns the predicted temperature at this location
     */
-  def makeGrid(temperatures: Iterable[(Location, Double)]): (Int, Int) => Double = {
-    ???
-  }
+  def makeGrid(temperatures: Iterable[(Location, Double)]): (Int, Int) => Double =
+    (lat, lon) => Visualization.predictTemperature(temperatures, Location(lat, lon))
 
   /**
     * @param temperaturess Sequence of known temperatures over the years (each element of the collection
@@ -20,7 +19,10 @@ object Manipulation {
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Double)]]): (Int, Int) => Double = {
-    ???
+    (lat, lon) => {
+      val (totalTemp, count) = temperaturess.map(xs => makeGrid(xs)(lat, lon)).map(x => (x, 1)).reduce((a, b) => (a._1 + b._1, a._2 + b._2))
+      totalTemp / count
+    }
   }
 
   /**
@@ -28,9 +30,8 @@ object Manipulation {
     * @param normals A grid containing the “normal” temperatures
     * @return A sequence of grids containing the deviations compared to the normal temperatures
     */
-  def deviation(temperatures: Iterable[(Location, Double)], normals: (Int, Int) => Double): (Int, Int) => Double = {
-    ???
-  }
+  def deviation(temperatures: Iterable[(Location, Double)], normals: (Int, Int) => Double): (Int, Int) => Double =
+    (lat, lon) => makeGrid(temperatures)(lat, lon) - normals(lat, lon)
 
 
 }
